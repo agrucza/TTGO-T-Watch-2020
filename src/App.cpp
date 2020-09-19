2c6a07acaa51011796d65a9ba33679e3a5d6dbea
Alexander Grucza <a.grucza@gmx.net>
2020-09-19 20:16:23 +0200

    UISideMenu: - new class for a side menu to show menu icons - this special container will have a settings icon/label as default/predefined child element
    App:
    - use of UISideMenu class instead of generic container

--- src/App.cpp
+++ src/App.cpp
@@ -5,10 +5,7 @@
 
 #include "GUI.h"
 #include "UIContainer.h"
-#include "UIElementLabel.h"
-#include "UIColors.h"
-#include "UIIcons.h"
-#include "fonts/IconsFontsSup18pt7b.h"
+#include "UISideMenu.h"
 
 extern GUI* gui;
 
@@ -22,15 +19,8 @@ App::App(String label, bool showHeader)
     _appContainer = new UIContainer(this, SIZE_ELEMENT, ALIGNMENT_HORIZONTAL);
     _appContainer->setPadding(0);
     
-    _menuContainer = new UIContainer(_appContainer);
+    _menuContainer = new UISideMenu(_appContainer);
     _appContainer->addUIElement(_menuContainer);
-    _menuContainer->setPadding(0);
-    
-    UIElementLabel* labelMenu1 = new UIElementLabel(UIICON_SUP_WRENCH, &IconsFontsSup18pt7b, _menuContainer);
-    _menuContainer->addUIElement(labelMenu1);
-    
-    UIElementLabel* labelMenu2 = new UIElementLabel(UIICON_SUP_TRASH_BIN, &IconsFontsSup18pt7b, _menuContainer);
-    _menuContainer->addUIElement(labelMenu2);
     
     _contentContainer = new UIContainer(_appContainer, SIZE_FULL);
     _appContainer->addUIElement(_contentContainer);
